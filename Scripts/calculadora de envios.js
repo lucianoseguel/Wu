78e2c54f366f24785e54f24a58f0611bfa6d7680
Luciano Seguel <96950349+lucianoseguel@users.noreply.github.com>
2024-06-01 23:21:52 -0300

    1.0

---
+++ Scripts/calculadora de envios.js
@@ -0,0 +1,51 @@
+
+
+
+
+let cuantoquieroenviar = document.getElementById('inputNumero')
+let valor = cuantoquieroenviar.value
+let resultadocalc = document.getElementById('resultado') 
+
+
+cuantoquieroenviar.addEventListener('input',function(){
+
+    const valor = parseFloat(cuantoquieroenviar.value)
+
+    if (valor < 2500000){
+    let acopiar = valor * 0.9429514091
+    acopiar = acopiar.toFixed(2)
+    resultadocalc.textContent = `A copiar en la casilla es ${acopiar}`
+    copynumber(acopiar);
+        
+}else if (valor >= 2500000) {
+    let acopiar = valor + 30250
+    acopiar = acopiar.toFixed(2)
+    resultadocalc.textContent = `A copiar en la casilla es ${acopiar}`
+    copynumber(acopiar.toFixed(2));
+
+} else if (valor == NaN) {
+    resultadocalc.textContent = `A copiar en la casilla es 0.00`
+}
+
+})
+
+
+
+
+
+
+
+
+resultadocalc.textContent = `El resultado es: ${acopiar}` ; 
+
+
+
+
+
+function copynumber(numero){ //Funcion del boton que copia el numero
+    navigator.clipboard.writeText(numero);
+    
+}
+
+
+
